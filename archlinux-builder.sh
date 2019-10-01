6ce80cc126d5d9896679d0a283c7251786093074
Hayao0819 <shun819.mail@gmail.com>
2019-10-01 18:04:51 +0900

    Add files via upload

--- archlinux-builder.sh
+++ archlinux-builder.sh
@@ -1,34 +1,46 @@
 #!/usr/bin/env bash
 
+
 ## 変数定義
 year=`date "+%Y"`
 month=`date "+%m"`
 day=`date "+%d"`
 
+
 ## 設定
-working_directory="/home/arch-build" # このディレクトリ内に設定ファイル等を作成するため空のディレクトリを指定することをおすすめします。
-image_file_name="archlinux-${year}.${month}.${day}-x86_64.iso" # それぞれ${yaer}、${month}、${day}で年、月、日に置き換えることができます。
+#このディレクトリ内に設定ファイル等を作成するため空のディレクトリを指定することをおすすめします。
+working_directory="/home/arch-build" 
+#フルパスで表記してください。それぞれ${yaer}、${month}、${day}で年、月、日に置き換えることができます。
+image_file_path="/home/archlinux-${year}.${month}.${day}-x86_64.iso" 
+#以下の設定は通常は変更しません。
+archiso_package_name="archiso" #pacaptのパッケージ名です。(AURのパッケージ名にする場合はAURHelperを有効化してください。)
+aur_helper="yay" #AURHelperの使用を強制する場合にのみpacmanから変更してください。もし存在しないAURHelperが入力された場合はpacmanが使用されます。また、AURHelperはpacmanと同じ構文のもののみ利用可能です。
+bluelog=0 #0=有効 1=無効 それ以外=有効
 
-## Rootチェック
-if [[ ! $UID = 0 ]]; then
-    echo "You need root permission." >&2
-    exit 1
-fi
-
-## ディストリビューションチェック
-if [[ -f /etc/os-release ]]; then
-    source /etc/os-release
-    if [[ ! $ID = "arch" ]]; then
-        echo "The script is able to run in ArchLinux only." >&2
-        exit 1
-    fi
-else
-    echo "There is not /etc/os-release."
-    exit 1
-fi
 
 ## 関数定義
-package_check () {
+function red_log () {
+    echo -e "\033[0;31m$@\033[0;39m" >&2
+    return 0
+}
+
+function blue_log () {
+    if [[ ! $bluelog = 1 ]]; then
+    echo -e "\033[0;34m$@\033[0;39m"
+    fi
+    return 0
+}
+
+function yellow_log () {
+    echo -e "\033[0;33m$@\033[0;39m" >&2
+    return 0
+}
+
+function package_check () {
+    if [[ -z $1 ]]; then
+        red_log "Please specify a package."
+        exit 1
+    fi
     if [[ -n $( pacman -Q | awk '{print $1}' | grep -x "$1" ) ]]; then
         return 0
     else
@@ -36,8 +48,81 @@ package_check () {
     fi
 }
 
-## ArchISOインストール
-if [[ $(package_check archiso) = 1 ]]; then
-    pacman -Syy
-    pacman -S archiso
+## Rootチェック
+if [[ ! $UID = 0 ]]; then
+    red_log "You need root permission." >&2
+    exit 1
 fi
+
+
+## ディストリビューションチェック
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    if [[ ! $ID = "arch" ]]; then
+        red_log "The script is able to run in ArchLinux only."
+        exit 1
+    fi
+else
+    red_log "There is not /etc/os-release."
+    exit 1
+fi
+
+
+## AUR Helperチェック
+if [[ $(package_check $aur_helper ; printf $? ) = 1 && ! $aur_helper = "pacman" ]]; then
+    pacman=pacman
+else
+    blue_log "Found AUR_Helper $aur_helper."
+    blue_log "Use $aur_helper"
+    pacman=$aur_helper
+fi
+
+## ArchISOインストール
+if [[ $(package_check $archiso_package_name ; printf $?) = 1 ]]; then
+    yellow_log "ArchISO is not installed."
+    yellow_log "Install ArchISO."
+    $pacman -Syy --noconfirm
+    $pacman -S --noconfirm archiso
+fi
+
+
+## 作業ディレクトリ作成
+if [[ ! -d $working_directory ]]; then
+    mkdir -p $working_directory
+    chmod 755 $working_directory
+else
+    printf "Working directory already exists. Do you want to initialize it? :"
+    read yn
+    alias del='rm -rf $working_directory'
+    case $yn in
+        y ) del ;;
+        Y ) del ;;
+        Yes ) del ;;
+        yes ) del ;;
+        * ) exit 1
+    esac
+    mkdir -p $working_directory
+    chmod 755 $working_directory
+fi
+
+if [[ ! -d $working_directory/out/ ]]; then
+    mkdir -p $working_directory/out/
+    chmod 755 $working_directory/out/
+fi
+
+## ArchISOプロファイルコピー
+if [[ -d /usr/share/archiso/configs/baseline/ ]]; then
+    cp -r /usr/share/archiso/configs/baseline/* $working_directory
+else
+    red_log "There is not ArchISO profiles."
+    red_log "Please Install ArchISO"
+    exit 1
+fi
+
+## ISO作成
+blue_log "Start building ArchLinux LiveCD."
+cd $working_directory
+./build.sh -v
+mv $working_directory/out/* $image_file_path
+blue_log "Created ArchLinux Live CD in $image_file_path"
+rm -r $working_directory
