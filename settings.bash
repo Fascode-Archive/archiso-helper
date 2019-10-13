18bc492df664bfbe1014166d2e63ae092184760b
hayao <shun819.mail@gmail.com>
2019-10-13 23:16:06 +0000

    設定項目追加に伴う設定ファイルの更新

--- settings.bash
+++ settings.bash
@@ -17,10 +17,8 @@ perm=664
 #対話時の自動返答（「yes」もしくは「no」空で自動返答を無効化）
 query=
 
-# archisoのパッケージ名です。(AURのパッケージ名にする場合はAURHelperを有効化してください。)
+# archisoのパッケージ名です。(AURのパッケージ名にする場合は事前にインストールしておいてください。)
 archiso_package_name="archiso"
-# AURHelperの使用を強制する場合にのみpacmanから変更してください。もし存在しないAURHelperが入力された場合はpacmanが使用されます。また、AURHelperはpacmanと同じ構文のもののみ利用可能です。
-aur_helper="pacman" 
 # メッセージの出力（0=有効 1=無効 それ以外=有効）
 bluelog=0 
 # これらの値を変更するとArchISOのバージョン判定が正常に行えなくなります。ArchISOのバージョンを固定する場合にのみ、両方の値を変更してください。（両方の値は必ず一致させてください。）
@@ -30,3 +28,9 @@ remote_archiso_version=
 i686_build_script=$current_scriput_dir/build_i686.sh
 # archisoの設定プロファイルへのパス
 archiso_configs="/usr/share/archiso/configs/releng"
+# Grubの背景（フルパスで記述してください。デフォルトは空です。）
+grub_background=
+# オーバーレイで追加するディレクトリ（ここで指定するディレクトリは/として認識されます。この値を設定する場合は非常に注意してください。デフォルトでは空です。）
+overlay_directory=
+# カスタムリポジトリのディレクトリ（このディレクトリ直下に必ずアーキテクチャ別のディレクトリを作成し、その中に*.pkg.tar.xzを配置してください。指定したパッケージをインストールする場合は必ずadd_pkgでパッケージ名を指定してください。）
+customrepo_directory=
