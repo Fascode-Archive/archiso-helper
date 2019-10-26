0b0bfdfee4a5e215c655008b5b058ade3b4aed43
hayao <shun819.mail@gmail.com>
2019-10-26 19:52:20 +0000

    URLを設定できるように変更

--- start_build.bash
+++ start_build.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+settings_url=https://raw.githubusercontent.com/Hayao0819/archlinux-latest-livecd-builder/master/settings.bash
+
+
 if [[ -f archlinux-builder.sh ]]; then
     rm archlinux-builder.sh
 fi
@@ -8,7 +11,7 @@ wget -O archlinux-builder.sh https://0e0.pw/Kviw
 if [[ -f settings.bash ]]; then
     rm settings.bash
 fi
-wget -O settings.bash https://0e0.pw/p-Ij
+wget -O settings.bash $settings_url
 
 chmod 775 $(pwd)/archlinux-builder.sh
 sudo $(pwd)/archlinux-builder.sh
