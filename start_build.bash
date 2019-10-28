836ee83dd4988d73597175163adb669dbd8497ce
hayao <shun819.mail@gmail.com>
2019-10-28 00:13:25 +0000

    コメントを追記

--- start_build.bash
+++ start_build.bash
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+## 設定ファイルのURL
 settings_url=https://raw.githubusercontent.com/Hayao0819/archlinux-latest-livecd-builder/master/settings.bash
 
+
+## 古いファイルの削除
+# archlinux-builder.sh
 if [[ -f /tmp/archlinux-builder.sh ]]; then
     rm /tmp/archlinux-builder.sh
 fi
-wget -O /tmp/archlinux-builder.sh https://0e0.pw/Kviw
-
+# settings.bash
 if [[ -f /tmp/settings.bash ]]; then
     rm /tmp/settings.bash
 fi
+
+
+## 新しいファイルのダウンロード
+wget -O /tmp/archlinux-builder.sh https://0e0.pw/Kviw
 wget -O /tmp/settings.bash $settings_url
 
+
+## 権限付与と実行
 chmod 775 /tmp/archlinux-builder.sh
 sudo tmp//archlinux-builder.sh
