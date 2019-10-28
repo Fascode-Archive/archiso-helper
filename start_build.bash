0b21fab85815ff35dd1b24b5e1975310cd3c9f03
hayao <shun819.mail@gmail.com>
2019-10-28 00:10:51 +0000

    /tmpにファイルをダウンロードするように変更

--- start_build.bash
+++ start_build.bash
@@ -2,16 +2,15 @@
 
 settings_url=https://raw.githubusercontent.com/Hayao0819/archlinux-latest-livecd-builder/master/settings.bash
 
-
-if [[ -f archlinux-builder.sh ]]; then
-    rm archlinux-builder.sh
+if [[ -f /tmp/archlinux-builder.sh ]]; then
+    rm /tmp/archlinux-builder.sh
 fi
-wget -O archlinux-builder.sh https://0e0.pw/Kviw
+wget -O /tmp/archlinux-builder.sh https://0e0.pw/Kviw
 
-if [[ -f settings.bash ]]; then
-    rm settings.bash
+if [[ -f /tmp/settings.bash ]]; then
+    rm /tmp/settings.bash
 fi
-wget -O settings.bash $settings_url
+wget -O /tmp/settings.bash $settings_url
 
-chmod 775 $(pwd)/archlinux-builder.sh
-sudo $(pwd)/archlinux-builder.sh
+chmod 775 /tmp/archlinux-builder.sh
+sudo tmp//archlinux-builder.sh
