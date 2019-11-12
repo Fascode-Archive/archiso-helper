6f45506eb91b9da5c78d2094ca69e048f18477b7
Hayao <shun819.mail@gmail.com>
2019-11-12 22:24:29 +0000

    出力先ディレクトリを指定可能にした

--- aur.bash
+++ aur.bash
@@ -34,6 +34,18 @@ if [[ ! -d $working_directory ]]; then
 fi
 
 
+# 出力先ディレクトリ
+if [[ -n $2 ]]; then
+    if [[ -d $2 ]]; then
+        export_directory=$2
+    else
+        export_directory=$(pwd)
+    fi
+else
+    export_directory=$(pwd)
+fi
+
+
 ## ビルド
 for (( i=0; i<number_of_pkg_aur ; i++ )); do
     # Clone
