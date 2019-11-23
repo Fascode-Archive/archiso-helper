e63a513bfd5218bb13fe35f9dd33b21ab0f57aa9
hayao <shun819.mail@gmail.com>
2019-11-23 22:41:03 +0000

    非公式リポジトリのアーキテクチャを自動で置き換えるように変更

--- archlinux-builder.sh
+++ archlinux-builder.sh
@@ -800,6 +800,7 @@ fi
 
 ## 非公式リポジトリの追加
 if [[ -n $add_repo_name ]]; then
+    arch=$make_arch
     echo -e "[$add_repo_name]\nSigLevel = $add_repo_siglevel\nServer = $add_repo_server" >> $working_directory/pacman.conf
 fi
 
