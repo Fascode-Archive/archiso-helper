51f026907f3255961b4fbf36fecf27e233764481
Hayao <shun819.mail@gmail.com>
2019-12-05 19:32:04 +0000

    それぞれのスクリプトを実行

--- run.sh
+++ run.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-
 script_dir=./scripts
-
 
 # 初期値読み込み
 source ./initial.sh
 
-
 # スクリプトを実行
-run () {
-    source $@
-}
-
+run () { source $@; }
 
 ## タイトル
 run $script_dir/start.sh
 
-
 ## 設定
 run $script_dir/load-settings.sh
 
+# ネット接続確認
+run $script_dir/check-network.sh
+
+# Rootチェック
+run $script_dir/check-root.sh
+
+# ディストリビューションチェック
+run $script_dir/check-os.sh
