c2fa08604304504db8c7f0af829961f9e06abe22
Hayao <shun819.mail@gmail.com>
2019-12-06 21:24:20 +0000

    翻訳スクリプトのインストール条件を追加

--- scripts/load-translate.sh
+++ scripts/load-translate.sh
@@ -1,5 +1,5 @@
 #!/usrbin/env bash
 
-if [[ $(package_check translate-shell ; printf $?) = 1 ]]; then
+if [[ $(package_check translate-shell ; printf $?) = 1 && ! $msg_language = "ja" ]]; then
     install_pacman translate-shell
 fi
