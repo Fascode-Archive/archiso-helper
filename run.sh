11dd2a79a174cb93e043999f4880ab8c539d57cc
Hayao <shun819.mail@gmail.com>
2019-12-11 16:41:13 +0000

    外部設定ファイルの読み込みを削除

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ run () { source $@; }
 run $script_dir/start.sh
 
 # 設定
-run $script_dir/load-settings.sh $1
+run $script_dir/load-settings.sh
 
 # シェル変数をグローバル変数へ
 run $script_dir/export-settings.sh
--- scripts/load-settings.sh
+++ scripts/load-settings.sh
@@ -6,37 +6,23 @@ month=$(date "+%m")
 day=$(date "+%d")
 current_scriput_path=$(cd $(dirname $0) && pwd)/$(basename $0)
 current_scriput_dir=$(pwd)
-make_arch=$(uname -m)
+arch=$(uname -m)
 
 
 ## 設定読み込み
 
-if [[ -n $1 ]]: then
-    if [[ -f $1 ]]: then
-        source $1
-        if [[ $? = 0 ]]; then
-            blue_log "外部設定ファイル$1が読み込まれました。"
-        else
-            red_log "外部設定ファイルの読み込みに失敗しました。"
-            exit_error
-        fi
+if [[ -f ./settings.conf ]]; then
+    source ./settings.conf
+    if [[ $? = 0 ]]; then
+        blue_log "settings.confが読み込まれました。"
     else
-        red_log "外部設定ファイルが存在しません。"
+        red_log "設定ファイルの読み込みに失敗しました。"
         exit_error
     fi
 else
-    if [[ -f ./settings.conf ]]; then
-        source ./settings.conf
-        if [[ $? = 0 ]]; then
-            blue_log "settings.confが読み込まれました。"
-        else
-            red_log "設定ファイルの読み込みに失敗しました。"
-            exit_error
-        fi
-    else
-        red_log "設定ファイルが見つかりませんでした。"
-    fi
+    red_log "設定ファイルが見つかりませんでした。"
 fi
+
 
 
 ## 設定後変数
